0a3d1165de0d1c5b4e0e9e799f27e978c8e1fcf2
Sergio Garrido <163670908+sergio-grr@users.noreply.github.com>
2024-10-13 18:17:48 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,50 @@
-// Obtener elementos del DOM
-const modal = document.getElementById('modal');
-const modalImage = document.getElementById('modalImage');
-const menuDiario = document.getElementById('menuDiario');
-const closeModal = document.querySelector('.close');
+window.onload = function() {
+    if (!sessionStorage.getItem('splashShown')) {
+        const splashScreen = document.getElementById('splash-screen');
+        const mainContent = document.getElementById('main-content');
 
-// Abrir el modal al hacer clic en la imagen
-menuDiario.onclick = function() {
-    modal.style.display = "block";
-    modalImage.src = this.src;
-}
+        setTimeout(function() {
+            splashScreen.style.opacity = '0';  
 
-// Cerrar el modal al hacer clic en la 'X'
-closeModal.onclick = function() {
-    modal.style.display = "none";
-}
+            setTimeout(function() {
+                splashScreen.style.display = 'none';
+                mainContent.style.display = 'block';
+                setTimeout(function() {
+                    mainContent.style.opacity = '1';  
+                }, 50);
+            }, 500);  
 
-// Cerrar el modal al hacer clic fuera del contenido
-window.onclick = function(event) {
-    if (event.target == modal) {
+            sessionStorage.setItem('splashShown', 'true');
+        }, 2000);  //
+    } else {
+       
+        document.getElementById('splash-screen').style.display = 'none';
+        document.getElementById('main-content').style.display = 'block';
+        setTimeout(function() {
+            document.getElementById('main-content').style.opacity = '1';  
+        }, 50);
+    }
+
+    // Funcionalidad del modal
+    const modal = document.getElementById('modal');
+    const modalImage = document.getElementById('modalImage');
+    const menuDiario = document.getElementById('menuDiario');
+    const closeModal = document.querySelector('.close');
+
+    menuDiario.onclick = function() {
+        modal.style.display = "block";
+        modalImage.src = this.src;
+    }
+
+
+    closeModal.onclick = function() {
         modal.style.display = "none";
     }
-}
+
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+};
